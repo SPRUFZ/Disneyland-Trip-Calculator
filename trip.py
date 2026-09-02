b5960d1deea7a23352c2a27ab2508b780e415d81
StudentPM <studentpm@Administrators-MacBook-Pro.local>
2026-09-02 15:29:22 -0700

    trip

--- trip.py
+++ trip.py
@@ -15,11 +15,40 @@ food_budget = float(input("how much will each person spend on food for the trip?
 food_budget_total = food_budget * party_size
 souvenir_budget = float(input("how much will each person spend on souvenirs for the trip? "))
 souvenir_budget_total = souvenir_budget * party_size
-distance_to_disneyland = float(input("What is the distance to DisneyLand in miles? "))
+distance_to_disneyland_oneway = float(input("What is the distance to DisneyLand in miles? "))
+round_trip_distance = distance_to_disneyland_oneway * 2
 vehicle_mpg = float(input("What is your vehicles MPG? "))
 current_gas_price = float(input("What is the current price of gas per gallon? "))
+gallons_needed = round_trip_distance / vehicle_mpg
+gas_cost = gallons_needed * current_gas_price
 Disneyland_parking_cost = float(input("What is the cost of parking at DisneyLand? "))
-Total_trip_budget = total_hotel_cost + total_ticket_cost + food_budget_total + souvenir_budget_total + (distance_to_disneyland / vehicle_mpg * current_gas_price) + Disneyland_parking_cost
-
+parking_price_per_day = Disneyland_parking_cost * days
+Total_trip_budget = total_hotel_cost + total_ticket_cost + food_budget_total + souvenir_budget_total + gas_cost + parking_price_per_day
+total_cost_per_person = Total_trip_budget / party_size
+total_cost_per_day = Total_trip_budget / days
+trip_budget = float(input("What is your budget for the trip? "))
+trip_budget_difference = trip_budget - Total_trip_budget
 print(f"Thank you for providing the information, {name}.")
-print(f"The total cost of your trip to DisneyLand will be: ${Total_trip_budget:.2f}")
+print()
+
+print(f"User's name: {name}")
+print(f"Number of people: {party_size}")
+print(f"Number of park days: {days}")
+print(f"Number of hotel nights: {days}")
+print(f"Park Hopper ticket price: ${ticket_cost_per_person:.2f}")
+print(f"Total Park Hopper cost: ${total_ticket_cost:.2f}")
+print(f"Total food cost: ${food_budget_total:.2f}")
+print(f"Total souvenir cost: ${souvenir_budget_total:.2f}")
+print(f"Total hotel cost: ${total_hotel_cost:.2f}")
+print(f"One-way driving distance: {distance_to_disneyland_oneway} miles")
+print(f"Round-trip driving distance: {round_trip_distance} miles")
+print(f"Vehicle MPG: {vehicle_mpg}")
+print(f"Gas price: ${current_gas_price:.2f}")
+print(f"Gallons of gas needed: {gallons_needed:.2f}")
+print(f"Total gas cost: ${gas_cost:.2f}")
+print(f"Total parking cost: ${parking_price_per_day:.2f}")
+print(f"Final Disneyland trip cost: ${Total_trip_budget:.2f}")
+print(f"Cost per person: ${total_cost_per_person:.2f}")
+print(f"Cost per park day: ${total_cost_per_day:.2f}")
+print(f"Trip budget: ${trip_budget:.2f}")
+print(f"Budget difference: ${trip_budget_difference:.2f}")
